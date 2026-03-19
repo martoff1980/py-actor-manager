9ab0a26deabc147bc89f6186821a639fa0e1aaf6
martoff1980 <42155051+martoff1980@users.noreply.github.com>
2026-03-19 12:13:43 +0200

    Update managers.py

--- app/managers.py
+++ app/managers.py
@@ -14,7 +14,8 @@ class ActorManager:
         self.cursor.execute(
             f"""
             CREATE TABLE IF NOT EXISTS {self.table_name}
-            (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT, last_name TEXT)
+            (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT, 
+            last_name TEXT)
             """
         )
         self.connection.commit()
